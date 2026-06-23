10ede45a3f609d4bace3cb0f06af95d8743f4e9a
Anish Vaidya <104675521+Anish0609@users.noreply.github.com>
2026-06-23 20:33:11 +0530

    Time: 0 ms (100%), Space: 19.5 MB (22.25%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,27 +5,25 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        def reverse(head,times):
+        def reverse(head,times) :
             curr=head
             prev=None
 
-            for i in range(times):
+            for i in range(times) :
                 nex=curr.next
                 curr.next=prev
                 prev=curr
                 curr=nex
-    
-        if head==None :
-            return head
             
-        left=head 
+        left=head
         prevleft=None
-        res=None
         size=2
+        res=None
 
         while True :
             right=left
-            for i in range (1,size):
+
+            for i in range(1,size) :
                 if right==None :
                     break
                 right=right.next
@@ -33,21 +31,26 @@ class Solution:
             if right :
                 nextleft=right.next
                 reverse(left,size)
+
                 if prevleft :
                     prevleft.next=right
                 prevleft=left
+
                 if res==None :
                     res=right
+                
                 left=nextleft
+            
             else :
                 if prevleft :
                     prevleft.next=left
                 if res==None :
-                    res=left    
+                    res=left
                 break
-        
+            
         return res
-                
+
+        
 
 
 
