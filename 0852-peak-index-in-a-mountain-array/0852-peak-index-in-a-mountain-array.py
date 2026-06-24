45d539297397c3aaa40247b2f6ab83bae404f24c
Anish Vaidya <104675521+Anish0609@users.noreply.github.com>
2026-06-24 18:58:39 +0530

    Time: 0 ms (100%), Space: 31.3 MB (73.9%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -2,12 +2,21 @@ class Solution:
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
         
         n=len(arr)
-        for i in range(n):
-            if i>0 :
-                if arr[i]>arr[i+1]:
-                    return i
-                    break
-                
+        low=0
+        high=n-1
+
+        while low<=high :
+            guess=(low+high)//2
+
+            if arr[guess]<arr[guess+1] :
+                low=guess+1
+            elif arr[guess]>arr[guess+1] :
+                res=guess
+                high=guess-1
+            
+        return res
+        
+
         
 
 
