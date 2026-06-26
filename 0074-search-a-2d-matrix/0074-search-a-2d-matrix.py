9920e676a7f405f9efa2a18ff215529c675ef441
Anish Vaidya <104675521+Anish0609@users.noreply.github.com>
2026-06-26 18:34:26 +0530

    Time: 0 ms (100%), Space: 19.7 MB (12.23%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,38 +1,37 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        
+        row=len(matrix)
+        col=len(matrix[0])
 
-        rows = len(matrix)
-        cols = len(matrix[0])
+        low=0
+        high=row-1
+        idx=-1
 
-        # Find the row
-        low, high = 0, rows - 1
-        idx = -1
+        while low<=high :
+            guess=(low+high)//2
 
-        while low <= high:
-            guess = (low + high) // 2
-
-            if matrix[guess][0] <= target <= matrix[guess][cols - 1]:
-                idx = guess
+            if matrix[guess][0]<=target<=matrix[guess][col-1] :
+                idx=guess
                 break
-            elif target < matrix[guess][0]:
-                high = guess - 1
-            else:
-                low = guess + 1
-
-        if idx == -1:
+            elif matrix[guess][col-1]<target :
+                low=guess+1
+            else :
+                high=guess-1
+            
+        if idx==-1 :
             return False
+        
+        low=0
+        high=col-1
 
-        # Search inside the row
-        low, high = 0, cols - 1
+        while low<=high :
+            guess=(low+high)//2
 
-        while low <= high:
-            guess = (low + high) // 2
-
-            if matrix[idx][guess] == target:
+            if matrix[idx][guess]==target :
                 return True
-            elif matrix[idx][guess] < target:
-                low = guess + 1
-            else:
-                high = guess - 1
-
+            elif matrix[idx][guess]<target :
+                low=guess+1
+            else :
+                high=guess-1
         return False
