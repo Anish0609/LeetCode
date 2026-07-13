63a47ad5eb914e63a47280e435699fe941cc9519
Anish Vaidya <104675521+Anish0609@users.noreply.github.com>
2026-07-13 22:18:19 +0530

    Time: 0 ms (100%), Space: 19.1 MB (95.55%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,28 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if not digits :
             return []
         
-        n=len(digits)
-
         keypad={"2":"abc", "3":"def", "4":"ghi", "5":"jkl", "6":"mno", "7":"pqrs", "8":"tuv", "9":"wxyz"}
 
         temp=[]
         res=[]
 
-        def fun(idx):
-            if idx==n :
+        def combo(idx) :
+            if idx==len(digits) :
                 res.append("".join(temp))
                 return
             
             for ch in keypad[digits[idx]] :
                 temp.append(ch)
-                fun(idx+1)
+                combo(idx+1)
                 temp.pop()
-
-
-        fun(0)
+            
+        combo(0)
         return res
+       
 
             
         
